643f4411c2fdfb526ebd0c4c30d56aaf7d26a99e
Vophan Lee <32331521+skyf0cker@users.noreply.github.com>
2018-07-12 08:12:05 +0800

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -2,9 +2,9 @@
 set timeout 30
 spawn sudo openconnect --juniper --script /etc/vpnc/vpnc-script https://sslvpn.sxu.edu.cn --servercert sha256:9293411775d8808b551ee2dadc9de27cc114372af0fbd5f84b451fdcfbe3afed
 expect "*的密码："
-send "326824\r"
+send "自己root的密码\r"
 expect "*username"
-send "201701003091\r"
+send "自己的学号\r"
 expect "*password"
-send "003091\r"
+send "自己的密码\r"
 interact
